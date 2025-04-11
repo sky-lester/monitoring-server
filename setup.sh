06ccea7dd179ccd57020b15b0777634377b25b98
lr <lr@devhubph.com>
2025-04-11 11:01:51 +0800

    install ansible via user only

--- setup.sh
+++ setup.sh
@@ -26,7 +26,7 @@ echo "Installing Ansible..."
 sudo dnf remove -y ansible
 sudo dnf install -y python3-pip
 pip3 install --upgrade pip
-pip3 install ansible
+pip3 install --user ansible
 
 # Download Prometheus
 PROM_VERSION="2.51.2"
